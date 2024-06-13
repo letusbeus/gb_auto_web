e183f40583d8d4ba6d9ecddcd8e87f9d2673ba94
letusbeus <letusbeus@mail.ru>
2024-06-13 18:40:40 +0300

    Added 'wait_for_element' method to explicitly wait for an element Removed calls to 'time.sleep', replaced by waiting for an element to appear using WebDriverWait. Added fixtures: - 'testdata' to retrieve data from the testadata.yaml file - 'site' to initialize the browser once for all tests in the module One common test was split into 2 separate steps - test_valid_credentials&test_create_post.

--- test_gb_teststand.py
+++ test_gb_teststand.py
@@ -1,46 +1,38 @@
 import time
 
-import yaml
-from module import Site
 
-with open('testdata.yaml') as f:
-    testdata = yaml.safe_load(f)
-site = Site(testdata["address"])
-
-
-def test_valid_credentials(x_login_selector, x_password_selector, btn_login_selector, x_hello_username,
-                           create_btn_selector, x_input_post_title, x_input_post_description, x_input_post_content,
-                           btn_save_post_selector, check_new_post_title):
-    asserts = []
-    input_login = site.find_element("xpath", x_login_selector)
+def test_valid_credentials(site, testdata, x_login_selector, x_password_selector, btn_login_selector, x_hello_username):
+    # Проверка авторизации
+    input_login = site.wait_for_element("xpath", x_login_selector)
     input_login.clear()
     input_login.send_keys(testdata["username"])
-    input_password = site.find_element("xpath", x_password_selector)
+    input_password = site.wait_for_element("xpath", x_password_selector)
     input_password.clear()
     input_password.send_keys(testdata["password"])
-    btn_login = site.find_element("css", btn_login_selector)
+    btn_login = site.wait_for_element("css", btn_login_selector)
     btn_login.click()
-    login_label = site.find_element("xpath", x_hello_username)
+    login_label = site.wait_for_element("xpath", x_hello_username)
     text = login_label.text.split(" ")
-    asserts.append(True if text[-1] == testdata["username"] else False)
-    btn_create_post = site.find_element("id", create_btn_selector)
+    assert text[-1] == testdata["username"], "Authorization failed"
+
+
+def test_create_post(site, testdata, create_btn_selector, x_input_post_title, x_input_post_description,
+                     x_input_post_content, btn_save_post_selector, check_new_post_title):
+    # Проверка создания нового поста
+    btn_create_post = site.wait_for_element("id", create_btn_selector)
     btn_create_post.click()
-    time.sleep(testdata["sleep_time"])
-    input_title = site.find_element("xpath", x_input_post_title)
+    input_title = site.wait_for_element("xpath", x_input_post_title)
     input_title.clear()
     input_title.send_keys(testdata["title"])
-    input_description = site.find_element("xpath", x_input_post_description)
+    input_description = site.wait_for_element("xpath", x_input_post_description)
     input_description.clear()
     input_description.send_keys(testdata["description"])
-    input_content = site.find_element("xpath", x_input_post_content)
+    input_content = site.wait_for_element("xpath", x_input_post_content)
     input_content.clear()
     input_content.send_keys(testdata["content"])
-    btn_save_post = site.find_element("xpath", btn_save_post_selector)
+    btn_save_post = site.wait_for_element("xpath", btn_save_post_selector)
     btn_save_post.click()
     time.sleep(testdata["sleep_time"])
-    post_label = site.find_element("xpath", check_new_post_title)
+    post_label = site.wait_for_element("xpath", check_new_post_title)
     text = post_label.text
-    asserts.append(True if text == testdata["title"] else False)
-    site.quit()
-    print(*asserts)
-    assert all(asserts), "FAIL"
+    assert text == testdata["title"], "Post creation failed"
