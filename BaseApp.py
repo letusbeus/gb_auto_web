e69d5da12c617a3bef7b0c04b32589f258f3fe0d
letusbeus <letusbeus@mail.ru>
2024-07-06 16:13:07 +0300

    Homework four: added API class

--- BaseApp.py
+++ BaseApp.py
@@ -1,13 +1,17 @@
 import logging
-
+import requests
+import yaml
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+with open("./testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
 
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = 'https://test-stand.gb.ru/'
+        self.base_url = testdata["address"]
 
     def go_to_site(self):
         try:
@@ -44,3 +48,24 @@ class BasePage:
         except:
             logging.exception('Exception with alert')
             return None
+
+
+class APIBasePage:
+    def __init__(self):
+        self.base_url = testdata["address"]
+        self.username = testdata["username"]
+        self.password = testdata["password"]
+
+    def login(self):
+        try:
+            result = requests.post(self.base_url + "/gateway/login",
+                                   data={"username": self.username, "password": self.password})
+        except:
+            logging.exception(f"Login fail exception")
+            return None
+        if result.status_code != 200:
+            logging.exception(f"Login failed with status code {result.status_code}")
+            return None
+        else:
+            logging.info(f"Login successful with status code {result.status_code}")
+        return result.json()["token"]
